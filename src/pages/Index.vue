1ce6b8e74f5bb0c12da039b2cebb9a1248897777
kimyuni <kimyuni@gim-yuniui-MacBookPro.local>
2020-11-02 12:48:39 +0900

    test

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,34 @@
 <template>
   <q-page class="">
-    <h1>yunibling</h1>
+    <div class="text-h1 q-pb-md shadow-1 cursor-pointer">yunibling</div>
+    <div class="box">
+      <q-btn color="red-3" text-color="black" label="button" icon="mail" outline :loading="loading" @click="onClick" />
+      <q-input v-model="text" @input="onInput" label="name" />
+    </div>
   </q-page>
 </template>
 
+<style lang="sass">
+.box
+  outline: 1px solid red
+</style>
+
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      loading: false,
+      text: ''
+    }
+  },
+  methods: {
+    onClick () {
+      this.loading = true
+    },
+    onInput () {
+      console.log(this.text)
+    }
+  }
 }
 </script>
